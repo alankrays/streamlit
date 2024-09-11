0f1fc466600fd241bb6c14c3e20d6d1d73b2c787
alankrays <guillaume.ciesielski1@gmail.com>
2024-09-11 15:54:38 +0200

    Update upload.py

--- tp/upload.py
+++ tp/upload.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import StringIO
-from vega_datasets import data
 
 uploaded_file = st.file_uploader("Choose a file",type="csv")
 if uploaded_file is not None:
