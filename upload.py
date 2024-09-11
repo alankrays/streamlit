16955465482e578862a8a639cb86924bebdb21ad
alankrays <guillaume.ciesielski1@gmail.com>
2024-09-11 16:39:22 +0200

    Update upload.py

--- upload.py
+++ upload.py
@@ -26,7 +26,7 @@ if uploaded_file is not None:
     user_selection = st.selectbox('Selectionnez une profession', dataframe.columns)
 
     arr = np.random.normal(1, 1, size=100)
-    ax,fig  = plt.subplots()
+    fig,ax  = plt.subplots()
     ax.hist(dataframe[user_selection], bins=60)
 
     st.pyplot(fig)
