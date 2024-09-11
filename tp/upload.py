45bbccd9a3e4c48bac5c3667f225a281cb2fc268
alankrays <guillaume.ciesielski1@gmail.com>
2024-09-11 15:46:23 +0200

    Update upload.py

--- tp/upload.py
+++ tp/upload.py
@@ -6,19 +6,19 @@ uploaded_file = st.file_uploader("Choose a file",type="csv")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+   # st.write(bytes_data)
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+    #st.write(stringio)
 
     # To read file as string:
     string_data = stringio.read()
-    st.write(string_data)
+    #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file,sep=',')
     st.write(dataframe)
 
-    user_selection = st.selectbox('Selectionnez une profession', dataframe.columns())
+    user_selection = st.selectbox('Selectionnez une profession', dataframe.columns)
 
