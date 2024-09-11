4166dd8d2b5fa2e7941e17b139848856e4b17139
alankrays <guillaume.ciesielski1@gmail.com>
2024-09-11 15:01:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import pandas as pd
 st.title("My Dashboard")
 df = pd.read_csv('data.csv',sep=',')
 
-if st.checkbox("Affichier le jdd"):
-  st.write(df)
+
 
 pro = df.Profession.unique()
 
 user_selection = st.selectbox('Selectionnez une profession', pro)
-st.write(df[df.Profession==user_selection])
+#st.write(df[df.Profession==user_selection])
 
 user_select1=st.slider("select your character", min_value=20,max_value=100,value=30,step=1)
 
-st.write(df[(df.Age==user_select1) & (df.Profession==user_selection)])
+#st.write(df[(df.Age==user_select1) & (df.Profession==user_selection)])
 
-
+if st.checkbox("Affichier le jdd"):
+  st.write(df[(df.Age==user_select1) & (df.Profession==user_selection)])
