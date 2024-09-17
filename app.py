037dd35dea6a525ef4accd2a287f8e1b6e9a583c
alankrays <guillaume.ciesielski1@gmail.com>
2024-09-17 16:14:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 import streamlit as st
 import pandas as pd
 
-st.title("My Dashboard")
-df = pd.read_csv('data.csv',sep=',')
+#st.title("My Dashboard")
+#df = pd.read_csv('data.csv',sep=',')
 
 
 
-pro = df.Profession.unique()
+#pro = df.Profession.unique()
 
-user_selection = st.selectbox('Selectionnez une profession', pro)
+#user_selection = st.selectbox('Selectionnez une profession', pro)
 #st.write(df[df.Profession==user_selection])
 
-user_select1=st.slider("select your character", min_value=20,max_value=100,value=30,step=1)
+#user_select1=st.slider("select your character", min_value=20,max_value=100,value=30,step=1)
 
 #st.write(df[(df.Age==user_select1) & (df.Profession==user_selection)])
 
-if st.checkbox("Affichier le jdd"):
-  st.write(df[(df.Age==user_select1) & (df.Profession==user_selection)])
+#if st.checkbox("Affichier le jdd"):
+ # st.write(df[(df.Age==user_select1) & (df.Profession==user_selection)])
+
+longitude = st.text_input(label, value="")
+st.write(longitude)
+#curl -X 'POST' \
+#  'https://0c73-34-32-169-73.ngrok-free.app/predict' \
+ # -H 'accept: application/json' \
+ # -H 'Content-Type: application/json' \
+ # -d '{
+ # "longitude": 0,
+ # "latitude": 0,
+ # "housing_median_age": 0,
+ # "total_rooms": 0,
+ # "total_bedrooms": 0,
+ # "population": 0,
+ # "households": 0,
+ # "median_income": 0
+#}'
