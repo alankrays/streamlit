45a5e9b941bc26c63cb62da5c7a054fe5f3fd188
alankrays <guillaume.ciesielski1@gmail.com>
2024-10-04 10:01:04 +0200

    Update monitoring.py

--- pages/monitoring.py
+++ pages/monitoring.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("monitoring")
+
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("gpt-3.5-turbo", "gpt-3", "gpt-4"),
+)
+
+st.write("You selected:", option)
