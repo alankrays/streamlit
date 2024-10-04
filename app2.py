4afb553e1bc8a14217913cddfd4923a8bd1c28f8
alankrays <guillaume.ciesielski1@gmail.com>
2024-10-04 10:48:05 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,7 +10,8 @@ x = requests.get(url)
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    #st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = x
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
