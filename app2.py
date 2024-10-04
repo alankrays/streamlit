779f969063927732dabec515c2e8f9d950a2f17e
alankrays <guillaume.ciesielski1@gmail.com>
2024-10-04 11:09:39 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,16 +4,17 @@ import requests
 
 
 st.title("FAQ")
-
+url = 'https://23fc-35-231-28-0.ngrok-free.app/model'
+x = requests.get(url)
+openai_model=x.text
 #st.write("You selected:", str(x.text))
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"] )
 
 if "openai_model" not in st.session_state:
-    url = 'https://23fc-35-231-28-0.ngrok-free.app/model'
-    x = requests.get(url)
+    
     st.write("You selected:", str(x.text))
     #st.session_state["openai_model"] = "gpt-3.5-turbo"
-    st.session_state["openai_model"] = str(x.text)
+    #st.session_state["openai_model"] = str(x.text)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -29,7 +30,8 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
+            #model=st.session_state["openai_model"],
+            model=openai_model
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
